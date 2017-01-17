0f25ae66f270da75626d2df987c0928bdcc6cbbb
nov <nov@matake.jp>
2017-01-17 17:31:56 +0900

    config for production

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module ConnectRpCertified
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.eager_load_paths += %W(#{config.root}/lib)
   end
 end
